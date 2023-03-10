8ca073db98b1becb44669deac6f59076a61e484a
ernest <ernest.wambugu@student.moringaschool.com>
2023-03-10 14:34:45 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+name = Aki
+
+function introduction (name) {
+    console.log (`Hi, my name is ${name}`);
+}
+ 
+
+function introductionWithLanguage (name, language) {
+    console.log (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+
+}
+/*function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }*/
+
+  function introductionWithLanguageOptional (name, language) {
+    console.log (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
